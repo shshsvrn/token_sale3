0c67e1ca7025d7f678f854e23b56a5dc1780949b
Ashish <ashishvirar@gmail.com>
2019-05-07 19:16:54 +0530

    3. Smoke Test

--- test/DappToken.js
+++ test/DappToken.js
@@ -8,7 +8,7 @@ contract('DappToken', function(accounts){
 			return tokenInstance.totalSupply();
 
 		}).then(function(totalSupply){
-			assert.equal(totalSupply.toNumber(), 9000000,'sets the total supply to 1,000,000');
+			assert.equal(totalSupply.toNumber(), 1000000,'sets the total supply to 1,000,000');
 		});
 	});
 })
